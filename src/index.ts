d74ac860ba83182fa6993c7009ac4b5071654578
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2025-01-06 18:33:38 -0300

    Arrumando porta

--- src/index.ts
+++ src/index.ts
@@ -10,4 +10,4 @@ server.get('/', (request: Request, response: Response) => {
     return response.status(200).json({ message: 'DioBank API' })
 })
 
-server.listen(5000, () => console.log('Server on'))
+server.listen(3333, () => console.log('Server on'))
