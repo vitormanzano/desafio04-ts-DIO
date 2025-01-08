0bcf9d5c7cbed5fd7f50b0b364fabde03c04ee69
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2025-01-08 19:30:07 -0300

    Revert "Primeira parte de typeorm"
    This reverts commit e175b02defe919df61f0ccdc2a5cff410b4e1ef2.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
-import 'reflect-metadata';
 import express, { Request, Response } from 'express';
 import { router } from './routes';
-import { AppDataSource } from './database';
 
 const server = express();
 
